236e6aec6ca3bba25357ecc9d3ac1508f1f20db1
MrRadChad <cfmckinney@gmail.com>
2019-05-04 13:50:08 -0600

    Redux Thunk and Async

--- src/Reducers/RootReducer.js
+++ src/Reducers/RootReducer.js
@@ -2,15 +2,16 @@ import {combineReducers} from 'redux'
 import {reducer as FormReducer} from 'redux-form';
 import testReducer from '../Components/Test/testReducer'
 import AuthReducer from '../Auth/AuthReducer'
-
 import bandReducer from '../bandList/bandReducer';
 import ModalReducer from '../Modals/ModalReducer';
+import AsyncReducer from '../Async/AsyncReducer';
 
 const rootReducer = combineReducers({
     form: FormReducer,
     bands: bandReducer,
     modals: ModalReducer,
-    authenticated: AuthReducer
+    authenticated: AuthReducer,
+    async: AsyncReducer
 })
 
 export default rootReducer
